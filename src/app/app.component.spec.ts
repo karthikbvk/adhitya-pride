374016a079b13e3124efba054c3199c736ec772c
karthik bobburi <osr.kittu@gmail.com>
2020-06-30 21:56:41 +0530

    name changes to adhitya

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'test1'`, () => {
+  it(`should have as title 'adhitya'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('test1');
+    expect(app.title).toEqual('adhitya');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('test1 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('adhitya app is running!');
   });
 });
